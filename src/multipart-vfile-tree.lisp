61fc6bb92af9b77cb891f8564cdab0aebc753162
Cayman <caymannava@gmail.com>
2015-10-13 23:59:23 -0700

    rearrange to better use vfile
    vfile-tree was changed to differentiate between directory and nondirectory
    nodes. The same is done here.

--- src/multipart-vfile-tree.lisp
+++ src/multipart-vfile-tree.lisp
@@ -4,8 +4,10 @@
   ((%use-headers-p
     :initarg :use-headers-p
     :accessor multipart-use-headers-p
-    :initform t)
-   (%boundary
+    :initform t)))
+
+(defclass multipart-vfile-directory-node (multipart-vfile-node vfile-directory-node)
+  ((%boundary
     :initarg :boundary
     :accessor multipart-vfile-boundary
     :initform (make-boundary))))
@@ -13,14 +15,17 @@
 (defmethod multipart-headers ((n multipart-vfile-node))
   (list
    `("Content-Disposition" . ,(format nil "file; filename=~S" (clean-path n)))
-   `("Content-Type" . ,(if (vfile-directory-p n)
-			   (format nil "multipart/mixed; boundary=~A" (multipart-vfile-boundary n))
-			   (format nil "application/octet-stream")))))
+   '("Content-Type" . "application/octet-stream")))
+
+(defmethod multipart-headers ((n multipart-vfile-directory-node))
+  (list
+   `("Content-Disposition" . ,(format nil "file; filename=~S" (clean-path n)))
+   `("Content-Type" . ,(format nil "multipart/mixed; boundary=~A" (multipart-vfile-boundary n)))))
 
 (defmethod multipart-stream ((n multipart-vfile-node))
   (vfile-open n))
 
-(defmethod vfile-directory-open ((n multipart-vfile-node) &rest rest)
+(defmethod vfile-open ((n multipart-vfile-directory-node) &rest rest)
   (apply #'make-multipart-stream (multipart-vfile-boundary n) (vfile-children n)))
 
 (defun make-multipart-vfile-tree (path &key (base (dirname path)) (recurse-p t) hidden-p)
@@ -28,14 +33,11 @@
 		       (lambda (file)
 			 (make-instance 'multipart-vfile-node
 					:base base
-					:directory-p nil
 					:history (list file)
-					:contents (pathname file)
-					:children nil))
+					:contents (pathname file)))
 		       (lambda (directory subfiles)
-			 (make-instance 'multipart-vfile-node
+			 (make-instance 'multipart-vfile-directory-node
 					:base base
-					:directory-p t
 					:history (list directory)
 					:contents (pathname directory)
 					:children subfiles))
--- src/package.lisp
+++ src/package.lisp
@@ -15,14 +15,15 @@
   (:import-from #:vfile
                 #:vfile-base
                 #:vfile-path
-                #:vfile-directory-p)
+                #:vfile-open)
   (:import-from #:vfile-tree
                 #:vfile-node
-                #:vfile-open
-                #:vfile-directory-open
+		#:vfile-directory-node
+                #:vfile-directory-p
                 #:vfile-children
                 #:traverse-filesystem)
   (:export #:multipart-vfile-node
+	   #:multipart-vfile-directory-node
            #:multipart-use-headers-p
            #:multipart-vfile-boundary
            #:make-multipart-vfile-tree))
